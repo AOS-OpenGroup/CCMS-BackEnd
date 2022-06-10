c6661e336b22b332485f18e8e4425fb140768098
jrenzoo <u202118568@upc.edu.pe>
2022-06-09 23:18:53 -0500

    feat: implemented methods for getting and creating musicians in services

--- src/main/java/com/ccms/musician/service/MusicianServiceImpl.java
+++ src/main/java/com/ccms/musician/service/MusicianServiceImpl.java
@@ -2,8 +2,8 @@ package com.ccms.musician.service;
 import com.ccms.musician.domain.model.entity.Musician;
 import com.ccms.musician.domain.persistence.MusicianRepository;
 import com.ccms.musician.domain.service.MusicianService;
-import com.ccms.studio.domain.persistence.StudioRepository;
-import com.ccms.studio.domain.service.StudioService;
+import com.ccms.shared.exception.ResourceNotFoundException;
+import com.ccms.shared.exception.ResourceValidationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -16,7 +16,7 @@ import java.util.Set;
 
 @Service
 public class MusicianServiceImpl implements MusicianService {
-    private static final String ENTITY = "Musicians";
+    private static final String ENTITY = "Musician";
     private final MusicianRepository musicianRepository;
 
     private final Validator validator;
@@ -25,25 +25,34 @@ public class MusicianServiceImpl implements MusicianService {
         this.validator = validator;
     }
 
-
     @Override
     public List<Musician> getAll() {
-        return null;
+        return musicianRepository.findAll();
     }
 
     @Override
     public Page<Musician> getAll(Pageable pageable) {
-        return null;
+        return musicianRepository.findAll(pageable);
     }
 
     @Override
     public Musician getById(Long musicianId) {
-        return null;
+        return musicianRepository.findById(musicianId).orElseThrow(()-> new ResourceNotFoundException(ENTITY, musicianId));
     }
 
     @Override
     public Musician create(Musician musician) {
-        return null;
+        Set<ConstraintViolation<Musician>> violations = validator.validate(musician);
+        if (!violations.isEmpty())
+            throw new ResourceValidationException(ENTITY, violations);
+
+        // Email Uniqueness validation
+        Musician musicianWithEmail = musicianRepository.findByEmail(musician.getEmail());
+        if (musicianWithEmail != null)
+            throw new ResourceValidationException(ENTITY,
+                    "A musician with the same email already exists.");
+
+        return musicianRepository.save(musician);
     }
 
     @Override
