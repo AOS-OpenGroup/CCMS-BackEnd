c49d6a3884ea511f9d328be3675c4ca867edc25b
Christian C <geolrco124@hotmail.com>
2022-06-23 00:06:09 -0500

    feat: Added Outbound Service Contracts.

---
+++ src/main/java/com/ccms/security/domain/persistence/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ccms.security.domain.persistence;
+
+import com.ccms.security.domain.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
