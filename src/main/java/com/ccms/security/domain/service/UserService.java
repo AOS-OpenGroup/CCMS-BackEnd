a1637030d115d51ef7a09e7d2fe99b01dd20930b
Christian C <geolrco124@hotmail.com>
2022-06-23 00:14:27 -0500

    feat: Added Domain Service Layer Contracts.

---
+++ src/main/java/com/ccms/security/domain/service/UserService.java
@@ -0,0 +1,15 @@
+package com.ccms.security.domain.service;
+
+import com.ccms.security.domain.model.entity.User;
+import com.ccms.security.domain.service.communication.AuthenticateRequest;
+import com.ccms.security.domain.service.communication.RegisterRequest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+import java.util.List;
+
+public interface UserService extends UserDetailsService {
+    ResponseEntity<?> authenticate(AuthenticateRequest request);
+    ResponseEntity<?> register(RegisterRequest request);
+    List<User> getAll();
+}
