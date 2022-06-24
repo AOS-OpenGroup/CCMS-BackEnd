8e2b72b88b2740410a9858d55c50ceaada92b247
Julio-GM <u201721569@upc.edu.pe>
2022-06-24 08:37:01 -0500

    feat: Implemented Owner Service

---
+++ src/main/java/com/ccms/users/owner/domain/service/OwnerService.java
@@ -0,0 +1,17 @@
+package com.ccms.users.owner.domain.service;
+
+import com.ccms.users.owner.domain.model.entity.Owner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface OwnerService {
+    List<Owner> getAll();
+    Page<Owner> getAll(Pageable pageable);
+    Owner getById(Long ownerId);
+    Owner create(Owner owner);
+    Owner update(Long ownerId, Owner request);
+    ResponseEntity<?> delete(Long ownerId);
+}
