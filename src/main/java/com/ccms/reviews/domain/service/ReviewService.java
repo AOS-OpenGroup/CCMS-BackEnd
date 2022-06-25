46f86d4baed44bd533ce9e63187e2dd1fa206d6b
Christian C <geolrco124@hotmail.com>
2022-06-25 00:26:39 -0500

    feat: Implementation Review Service Implementation.

---
+++ src/main/java/com/ccms/reviews/domain/service/ReviewService.java
@@ -0,0 +1,17 @@
+package com.ccms.reviews.domain.service;
+
+import com.ccms.reviews.domain.model.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ReviewService {
+    List<Review> getAll();
+    Page<Review> getAll(Pageable pageable);
+    Review getById(Long reviewId);
+    Review create(Review review);
+    Review update(Long reviewId, Review request);
+    ResponseEntity<?> delete(Long reviewId);
+}
