2c470118b88f2c9bed9f35c3c736fb7639160fcd
Raul Ventura <u201714049@upc.edu.pe>
2022-06-25 07:06:16 -0500

    feat: Implemented Booking Class

--- src/main/java/com/ccms/studio/domain/model/entity/Booking.java
+++ src/main/java/com/ccms/studio/domain/model/entity/Booking.java
@@ -1,4 +1,35 @@
 package com.ccms.studio.domain.model.entity;
+import com.ccms.shared.domain.model.AuditModel;
+import lombok.*;
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
-public class Booking {
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "bookings")
+public class Booking extends AuditModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Id
+    private Long musicianId;
+
+    @Id
+    private Long StudioId;
+
+    @NotNull
+    @NotBlank
+    private String date;
+
+    @NotNull
+    @NotBlank
+    private String time;
+
 }
